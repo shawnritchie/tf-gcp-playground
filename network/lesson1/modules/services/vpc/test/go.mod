df448f7e523265b720c689aa6bd75fcf616a796a
Shawn Ritchie <xritchie@gmail.com>
2021-11-16 21:08:25 +0100

    Added multi stage vpc test

---
+++ network/lesson1/modules/services/vpc/test/go.mod
@@ -0,0 +1,9 @@
+module github.com/shawnritchie/tf-gcp-playground
+
+go 1.15
+
+require (
+	github.com/go-ping/ping v0.0.0-20211014180314-6e2b003bffdd
+	github.com/gruntwork-io/terratest v0.38.4
+	github.com/stretchr/testify v1.7.0
+)
--- network/lesson1/modules/services/vpc/test/vpc_test.go
+++ network/lesson1/modules/services/vpc/test/vpc_test.go
@@ -1,34 +1,111 @@
 package test
 
 import (
-	"testing"
-
+	"fmt"
+	"github.com/go-ping/ping"
+	"github.com/gruntwork-io/terratest/modules/retry"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	t "github.com/gruntwork-io/terratest/modules/testing"
 	"github.com/stretchr/testify/assert"
+	"log"
+	"regexp"
+	"testing"
+	"time"
 )
 
-func TestTerraformComputeResourceExample(t *testing.T) {
+var (
+	SSHMaxRetries = 20
+	// we don't want to retry for too long, but we should do it at least a few times to make sure the instance is up
+	SSHMaxRetriesExpectError = 3
+	SSHSleepBetweenRetries   = 5 * time.Second
+	SSHTimeout               = 15 * time.Second
+	SSHEchoText              = "Hello World"
+)
+
+func TestTerraformVPCResourceExample(t *testing.T) {
 	t.Parallel()
 
-	opts := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		// website::tag::1::Set the path to the Terraform code that will be tested.
-		// The path to where our Terraform code is located
-		TerraformDir: "../examples",
+	//os.Setenv("SKIP_bootstrap", "true")
+	//os.Setenv("SKIP_deploy", "true")
+	//os.Setenv("SKIP_test_setup", "true")
+	//os.Setenv("SKIP_teardown", "true")
+	//os.Setenv("SKIP_test_icmp", "true")
 
-		// Variables to pass to our Terraform code using -var-file options
-		VarFiles: []string{"varfile.tfvars"},
+	vpcExampleDir := test_structure.CopyTerraformFolderToTemp(t, "../../", "vpc/examples")
+
+	test_structure.RunTestStage(t, "bootstrap", func() {
+
+		opts := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+			// website::tag::1::Set the path to the Terraform code that will be tested.
+			// The path to where our Terraform code is located
+			TerraformDir: vpcExampleDir,
+
+			// Variables to pass to our Terraform code using -var-file options
+			VarFiles: []string{"varfile.tfvars"},
+		})
+
+		test_structure.SaveTerraformOptions(t, vpcExampleDir, opts)
+		test_structure.SaveString(t, vpcExampleDir, "LIBA", "fniek")
 	})
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer terraform.Destroy(t, opts)
+	defer test_structure.RunTestStage(t, "teardown", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, vpcExampleDir)
+		terraform.Destroy(t, terraformOptions)
+	})
 
-	// website::tag::2::Run "terraform init" and "terraform apply".
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
-	terraform.InitAndApply(t, opts)
+	test_structure.RunTestStage(t, "deploy", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, vpcExampleDir)
+		terraform.InitAndApply(t, terraformOptions)
+	})
 
-	// Run `terraform output` to get the values of output variables
-	actualComputeResourceId := terraform.Output(t, opts, "computer_resource_id")
+	test_structure.RunTestStage(t, "test_setup", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, vpcExampleDir)
 
-	assert.NotNil(t, actualComputeResourceId, "computer_resource_id should not be null")
-	assert.NotEmpty(t, actualComputeResourceId, "computer_resource_id should not empty")
+		assertValidResourceId(t, terraformOptions, "us_a_vpc_id")
+		assertValidResourceId(t, terraformOptions, "us_b_vpc_id")
+		assertValidResourceId(t, terraformOptions, "us_a_computer_resource_id")
+		assertValidResourceId(t, terraformOptions, "us_b_computer_resource_id")
+	})
+
+	test_structure.RunTestStage(t, "test_icmp", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, vpcExampleDir)
+		publicIP := terraform.Output(t, terraformOptions, "us_b_nat")
+		re := regexp.MustCompile("\\[(.*)\\]")
+		match := re.FindStringSubmatch(publicIP)
+
+		fmt.Println(match[1])
+		log.Printf("IP: %#v", match[1])
+
+		pinger, err := ping.NewPinger(match[1])
+		if err != nil {
+			t.Error(err)
+		}
+
+		pinger.Count = 3
+		err = pinger.Run()
+		if err != nil {
+			t.Error(err)
+		}
+
+		stats := pinger.Statistics()
+
+		log.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n",
+			stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
+		assert.True(t, stats.PacketsRecv > 0, "at least single successfully ping")
+	})
+}
+
+func assertValidResourceId(t t.TestingT, options *terraform.Options, key string) {
+	extractedResource := terraform.Output(t, options, key)
+
+	assert.NotNil(t, extractedResource, fmt.Sprintf("%s should not be null", key))
+	assert.NotEmpty(t, extractedResource, fmt.Sprintf("%s should not empty", key))
+}
+
+func doWithRetryAndTimeoutE(t *testing.T, description string, maxRetries int, sshSleepBetweenRetries time.Duration, timeoutPerRetry time.Duration, action func() (string, error)) (string, error) {
+	return retry.DoWithRetryE(t, description, maxRetries, sshSleepBetweenRetries, func() (string, error) {
+		return retry.DoWithTimeoutE(t, description, timeoutPerRetry, action)
+	})
 }
