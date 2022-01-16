9e7b33a9a809eabd836636ab109911232b60b3b7
Shawn Ritchie <xritchie@gmail.com>
2022-01-16 21:47:44 +0100

    Added autoscalling group to output which will most likely need for the automated tests

--- network/lesson1/modules/services/load-balancer/examples/internal/regional/https/output.tf
+++ network/lesson1/modules/services/load-balancer/examples/internal/regional/https/output.tf
@@ -1,3 +1,19 @@
+output "project_id" {
+  value = module.host_project_container.project_id
+}
+
+output "region" {
+  value = module.asg_us.region
+}
+
+output "instance_group_id" {
+  value = module.asg_us.instance_group_id
+}
+
+output "instance_group_name" {
+  value = module.asg_us.instance_group_name
+}
+
 output "public_ip" {
   value = module.private_ip.address
 }
